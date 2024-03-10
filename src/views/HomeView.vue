e9b8338d8d075ecce9afe2453c5dcf4d65aa6873
tyy-sss <3127023395@qq.com>
2024-03-10 09:48:47 +0800

    three.js案例

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,10 @@
-<script lang="ts" setup>
-import { Decoration1, Decoration2 } from "@kjgl77/datav-vue3";
-</script>
+
 <template>
   <!-- 引入之后就可以在vue的template中直接使用 -->
-  <decoration-1
-    :color="['pink', 'yellow']"
-    style="width: 200px; height: 50px"
-  />
-  <decoration-2 :reverse="true" style="width: 5px; height: 150px" />
   <dv-border-box-1 class="container" :color="['red']"> </dv-border-box-1>
 </template>
+<script lang="ts" setup>
+</script>
 <style>
 .container {
   height: 100px;
