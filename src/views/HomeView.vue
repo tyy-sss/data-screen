6a5c6def8862e2d8f478e3a9bceade54f40e0972
3403784285-lucky <147294730+3403784285-lucky@users.noreply.github.com>
2024-03-10 09:58:32 +0800

    做了一下改善

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,24 @@ import { BorderBox11 as DvBorderBox11, BorderBox7 as DvBorderBox7 } from '@kjgl7
           <div class="pro-list" v-for="i in 5" :key="i">
             <div class="li-container">
               <dv-decoration-9 class="pro-concrete" style="width:90px;height:90px;">{{ i }}</dv-decoration-9>
-              <dv-border-box-8 class="pro-getScore" style="width:240px;height:80px;"></dv-border-box-8>
+              <dv-border-box-8 class="pro-getScore" style="width:240px;height:80px;">
+                <table class="score-table">
+                  <tr>
+                    <th>答题题序</th>
+                    <th>得分</th>
+                    <th>答题时间</th>
+
+                  </tr>
+
+                  <tr>
+                    <td>第一个答题</td>
+                    <td>222</td>
+                    <td>11:00</td>
+
+
+                  </tr>
+                </table>
+              </dv-border-box-8>
 
             </div>
 
@@ -26,6 +43,8 @@ import { BorderBox11 as DvBorderBox11, BorderBox7 as DvBorderBox7 } from '@kjgl7
             <dv-decoration-2 class="team-border" style="width:400px;height:5px;" />
             <img class="team-logo"
               src="https://tse3-mm.cn.bing.net/th/id/OIP-C.oC9ZmasIzoCw0Bo2cpmsgAHaNK?w=115&h=187&c=7&r=0&o=5&dpr=2&pid=1.7">
+              <dv-decoration-7 class="team-information" style="width:150px;height:30px;" :color="['rgba(256,256,256,0.4),rgba(256,256,256,0.4)','white']">    队伍5    </dv-decoration-7>
+
             <dv-decoration-2 class="team-border" style="width:400px;height:5px;" />
           </div>
           <div class="team-second">
@@ -37,146 +56,171 @@ import { BorderBox11 as DvBorderBox11, BorderBox7 as DvBorderBox7 } from '@kjgl7
           <div class="team-third">
             <div class="team-third-title">排名变化曲线</div>
 
-         <lineChart></lineChart>
+            <lineChart></lineChart>
           </div>
         </dv-border-box-7></div>
       <div class="third region">
-   
-          <dv-border-box-7 class="rank-top" :color="['rgba(41,148,187,0.4)']">
-            <dv-decoration-7 class="team-log" style="width:150px;height:30px;">日志信息</dv-decoration-7>
 
-          </dv-border-box-7>
-          <dv-border-box-7 class="rank-bottom" :color="['rgba(41,148,187,0.4)']">
-            <dv-decoration-7 class="get-rw" style="width:150px;height:30px;">rw总得分</dv-decoration-7>
+        <dv-border-box-7 class="rank-top" :color="['rgba(41,148,187,0.4)']">
+          <dv-decoration-7 class="team-log" style="width:150px;height:30px;">日志信息</dv-decoration-7>
+
+        </dv-border-box-7>
+        <dv-border-box-7 class="rank-bottom" :color="['rgba(41,148,187,0.4)']">
+          <dv-decoration-7 class="get-rw" style="width:150px;height:30px;">rw总得分</dv-decoration-7>
 
         </dv-border-box-7>
       </div>
 
     </div>
-  
+
   </dv-full-screen-container>
- 
+
 
 
 </template>
 
 <style lang="less" scoped>
-.rank-top {
-  width: 390px;
-  height: 300px;
-  margin-bottom: 20px;
-}
-
-.rank-bottom {
-
-  width: 390px;
-  height: 480px;
-}
-
-
-.team-second-title, .team-third-title {
+.score-table {
   color: white;
-  font-size: 19px;
-  margin-left: 45px;
+  font-size: 14px;
+  margin: 4px auto;
+
+
+
+  th{
+    padding: 6px;
+    color: rgb(79, 209, 221);
+  }
+  td {
+
+    padding: 6px;
+  }
 }
 
-.team-view {
-  margin-left: 150px;
-  margin-top: 30px;
-  margin-bottom: 20px;
-  
-}
 
-.team-border {
-  margin-left: 100px;
-}
+  .rank-top {
+    width: 390px;
+    height: 300px;
+    margin-bottom: 20px;
+  }
 
-.team-first {
-  margin-left: 40px;
-  margin-bottom: 30px;
-}
+  .rank-bottom {
 
-.team-logo {
-  height: 80px;
-  width: 80px;
-
-}
-
-.team-information, .team-log, .get-rw {
-  color: white;
-  font-weight: 800;
-  margin-left: 10px;
-  font-size: 18px;
-
-  margin-top: 10px;
-  ;
-  margin-bottom: 30px;
-}
-
-.pro-getScore {
-  margin-left: 30px;
-  ;
-  margin-top: 10px;
-  ;
-}
-
-.li-container {
-  display: flex;
-}
-
-.pro-concrete {
-  left: 10px;
-
-}
-
-.pro-list {
-  color: white;
-  margin-top: 57px;
-
-}
-
-.CTF-title {
-  margin: 10px 0;
-  color: white;
-  font-weight: 800;
-  font-size: 18px;
-  margin-bottom: 30px;
-}
-
-.frame-container {
-  display: flex;
-}
-
-.container {
-  height: 100px;
-}
-
-.border-frame {
-  position: absolute;
+    width: 390px;
+    height: 480px;
+  }
 
 
-}
+  .team-second-title,
+  .team-third-title {
+    color: white;
+    font-size: 19px;
+    margin-left: 45px;
+  }
 
-.region {
-  margin-top: 70px;
-  ;
-}
+  .team-view {
+    margin-left: 150px;
+    margin-top: 30px;
+    margin-bottom: 20px;
 
-.first {
-  width: 390px;
-  height: 800px;
-  margin-left: 20px;
-}
+  }
 
-.third {
-  width: 390px;
-  height: 800px;
-  margin-left: 20px;
-}
+  .team-border {
 
-.second {
-  width: 570px;
-  height: 800px;
-  margin-left: 20px;
-}
-</style>
+    position: absolute;
+    left: 100px;
+  }
+
+
+  .team-first {
+    margin-left: 40px;
+    margin-bottom: 30px;
+    position: relative;
+    display: flex;
+  }
+
+  .team-logo {
+    height: 80px;
+    width: 80px;
+
+  }
+
+  .team-information,
+  .team-log,
+  .get-rw {
+    color: white;
+    font-weight: 800;
+    margin-left: 10px;
+    font-size: 18px;
+
+    margin-top: 10px;
+    ;
+    margin-bottom: 30px;
+  }
+
+  .pro-getScore {
+    margin-left: 30px;
+    margin-top: 10px;
+    box-sizing: border-box;
+
+  }
+
+  .li-container {
+    display: flex;
+  }
+
+  .pro-concrete {
+    left: 10px;
+
+  }
+
+  .pro-list {
+    color: white;
+    margin-top: 57px;
+
+  }
+
+  .CTF-title {
+    margin: 10px 0;
+    color: white;
+    font-weight: 800;
+    font-size: 18px;
+    margin-bottom: 30px;
+  }
+
+  .frame-container {
+    display: flex;
+  }
+
+  .container {
+    height: 100px;
+  }
+
+  .border-frame {
+    position: absolute;
+
+
+  }
+
+  .region {
+    margin-top: 70px;
+    ;
+  }
+
+  .first {
+    width: 390px;
+    height: 800px;
+    margin-left: 20px;
+  }
+
+  .third {
+    width: 390px;
+    height: 800px;
+    margin-left: 20px;
+  }
+
+  .second {
+    width: 570px;
+    height: 800px;
+    margin-left: 20px;
+  }</style>
